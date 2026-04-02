cf9473f1e778e970ed7ac8c255a3aa1ae8917794
John-Peter Mbock <lipemlidjob.junior@ictuniversity.edu.cm>
2026-04-02 23:30:23 +0100

    :

---
+++ day3b.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int age;
+	cout << "Hey, how old are u?\n";
+	cin >> age;
+	cout << "Cool, u're " << age << " years old\n";
+	return 0;
+}
