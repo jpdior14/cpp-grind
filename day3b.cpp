94c570a91754c0c226a699b76741d7ec2239f717
John-Peter Mbock <lipemlidjob.junior@ictuniversity.edu.cm>
2026-04-03 00:04:46 +0100

    Age

--- day3b.cpp
+++ day3b.cpp
@@ -5,6 +5,17 @@ int main() {
 	int age;
 	cout << "Hey, how old are u?\n";
 	cin >> age;
-	cout << "Cool, u're " << age << " years old\n";
+	if (age < 18) {
+		cout << "Bro is a literal prodigy.\n";
+	} else if (age == 18) {
+		cout << "Welcome to adulthood, lock in!\n";
+	} else {
+		cout << "Unc status. Too late for you lolll\n";
+	}
+
+	for (int i = 0; i < age; i++) {
+		cout << "I'm gonna be the BEST in the tech industry!\n";
+	}
+
 	return 0;
 }
