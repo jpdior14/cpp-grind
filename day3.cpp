f9dbcb85ea3d58456a8942c420b9ceb8b14a3267
John-Peter Mbock <lipemlidjob.junior@ictuniversity.edu.cm>
2026-04-02 22:21:16 +0100

    Ledger

---
+++ day3.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <fstream>
+using namespace std;
+
+double getAverage(const vector<double>& data) {
+	if (data.empty()) return 0.0;
+
+	double sum = 0;
+	for (double val : data) {
+		sum += val;
+	}
+	return sum / data.size();
+}
+
+double getNorm(const vector<double>& data) {
+	double sum_sq = 0;
+	for (double val : data) {
+		sum_sq += val * val;
+	}
+	return sqrt(sum_sq);
+}
+
+void printReport(const vector<double>& data) {
+	cout << "---------------------\n";
+	cout << "REPORT GENERATED\n";
+
+	if (data.empty()) {
+		cout << "No data entered.\n";
+		return;
+	}
+
+	double avg = getAverage(data);
+	double norm = getNorm(data);
+
+	cout << "Total Inputs: " << data.size() << endl;
+	cout << "Average: " << avg << endl;
+	cout << "Magnitude (Norm): " << norm << endl;
+
+	cout << "History: ";
+	for (double val : data) {
+		cout << val << " ";
+	}
+	cout << "\n";
+	cout << "Shutting down ...  Bye!\n";
+	cout << "---------------------\n";
+}
+
+void saveToDisk(const vector<double>& data) {
+	ofstream file("history.txt");
+
+	for (double val : data) {
+		file << val << endl;
+	}
+	file.close();
+}
+
+void loadFromDisk(vector<double>& data) {
+	ifstream file("history.txt");
+
+	if (!file.is_open()) {
+		return;
+	}
+
+	double val;
+	while (file >> val) {
+		data.push_back(val);
+	}
+	file.close();
+}
+
+int main() {
+	vector<double> history;
+
+	loadFromDisk(history);
+	if (!history.empty()) {
+		cout << "[SYSTEM] Previous session loaded. " << history.size() << " entries found.\n";
+	}
+
+	double x;
+
+	while (true) {
+		cout << "\nEnter a number (999 to exit): ";
+		cin >> x;
+
+		if (x == 999) {
+			printReport(history);
+			saveToDisk(history);
+			break;
+		}
+
+		history.push_back(x);
+	}
+
+	return 0;
+}
