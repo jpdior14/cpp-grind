57722865c02b3c2e629b5ad1704e20a99fb27118
John-Peter Mbock <lipemlidjob.junior@ictuniversity.edu.cm>
2026-04-03 23:55:55 +0100

    My array

---
+++ day4b.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	cout << "Day 1: C++ is raw power. I'm locked in!\n";
+
+	int array[4] = {2, 7, 11, 15};
+
+	cout << "The first number in my array is: " << array[0] << "\n";
+
+	return 0;
+}
