1d26cc36cf0f7d10304d77dcd423c9670d67841d
John-Peter Mbock <lipemlidjob.junior@ictuniversity.edu.cm>
2026-04-06 20:06:28 +0100

    Two Sum

--- day003.cpp
+++ day003.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
 #include <vector>
-using namespace std;
 
 int main() {
-    vector<int> nums = {2, 7, 11};
-    int target = 9;
-    
-    for (int i = 0; i < nums.size(); i++) {
-        for (int j = i + 1; j < nums.size(); j++) {
-            if (nums[i] + nums[j] == target) {
-                cout << "FOUND IT!" << endl << "Indices: " << i << " and " << j << endl;
+	std::vector<int> nums = {2, 7, 11};
+	int target = 9;
+
+	for (int i = 0; i < nums.size(); i++) {
+		for (int j = i + 1; j < nums.size(); j++) {
+			if (nums[i] + nums[j] == target) {
+				std::cout << "FOUND IT!\nIndices: " << i << " and " << j << '\n';
             }
         }
     }
