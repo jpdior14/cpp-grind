9d88a845568cbe80cd4a7a16f1be234835319a3f
John-Peter Mbock <lipemlidjob.junior@ictuniversity.edu.cm>
2026-04-11 18:54:06 +0100

    The 996 Log

---
+++ day1g.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+	std::cout << "Day 1. I am locked in.\n";
+
+	std::vector<int> grind_count = {1, 2, 3, 4, 5};
+
+	std::cout << "Days until I'm a Senior Engineer: " << grind_count.size() << '\n';
+
+	return 0;
+}
