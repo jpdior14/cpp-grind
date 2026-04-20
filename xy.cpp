da1b30bcf17cb1ac1c8b4d34adc9e12ca5c34ded
John-Peter Mbock <lipemlidjob.junior@ictuniversity.edu.cm>
2026-04-20 23:57:50 +0100

    XY

---
+++ xy.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello, World!\n";
+	
+	int x = 10;
+	int* y = &x;
+
+	cout << "x = " << x << "\ny = " << y << endl;
+	return 0;
+}
