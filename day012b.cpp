8afa114d3f7ea76537c172338c4f1073f1246b61
John-Peter Mbock <lipemlidjob.junior@ictuniversity.edu.cm>
2026-04-30 22:32:42 +0100

    The Baidu Apollo Driving Engine

--- day012b.cpp
+++ day012b.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <memory> // NEW HARDWARE LIBRARY UNLOCKED! Brings in the Elite Guards!
+#include <memory>
 
 class Apollo_AI_Module {
 public:
@@ -7,33 +7,26 @@ public:
         std::cout << "[APOLLO AI]: Core navigation module loaded directly into DDR5 RAM!\n";
     }
 
-    void process_video_feed() {
-        std::cout << "--> AI Scanning 360-degree LiDAR cameras... Road clear.\n";
-    }
-
     ~Apollo_AI_Module() {
         std::cout << "[APOLLO AI FATAL OVERRIDE]: RAM Purged automatically. Target deactivated.\n";
+    }
+
+    void process_vid_feed() {
+        std::cout << "--> AI Scanning 360-degree LiDAR cameras... Road clear.\n";
     }
 };
 
 int main() {
     std::cout << "=== BAIDU DRIVING SYSTEMS: INITIATE TEST RUN ===\n\n";
 
-    // 1. OPENING A LIMITED SCOPE
-    // This is like driving the car for a trip. Everything happens in these brackets!
     {
         std::cout << "[Starting Engine Phase]\n";
 
-        // 2. THE SMART POINTER! (std::make_unique)
-        // Read this out loud: "I am assigning an elite guard (unique_ptr) of type Apollo_AI_Module 
-        // to securely build and watch my new memory in the heap!" 
-        // Notice there is NO "new" keyword anymore! 'std::make_unique' is far safer!
         std::unique_ptr<Apollo_AI_Module> autopilot_drone = std::make_unique<Apollo_AI_Module>();
-
-        autopilot_drone->process_video_feed(); // Note the '->' operator for pointer function calls!
+        autopilot_drone->process_vid_feed();
 
         std::cout << "[Journey completed. Driver stepping out... Watch the auto-purge below!]\n";
-    } // THE EXACT NANOSECOND IT HITS THIS BRACKET, THE SMART POINTER PULLS THE TRIGGER AND CLEANS UP!
+    }
 
     std::cout << "\nStatus: ZERO 'delete' KEYWORDS TYPED. ZERO LEAKS CREATED. AUTOMATED ARCHITECTURE W.\n";
 
