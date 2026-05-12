1b474d5958f9fd3dff9979ee6967f42e2c3eadc6
John-Peter Mbock <lipemlidjob.junior@ictuniversity.edu.cm>
2026-05-12 23:43:33 +0100

    Grade Manager v2.0

---
+++ functions.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+int calculateSum(int scores[], int size) {
+	int sum {0};
+	for (int i {0}; i < size; i++) {
+		sum += scores[i];
+	}
+	return sum;
+}
+
+int calculateAverage(int scores[], int size) {
+	int sum = calculateSum(scores, size);
+	return sum / size;
+}
+
+int findHighest(int scores[], int size) {
+	int highest = scores[0];
+	for (int i {1}; i < size; i++) {
+		if (scores[i] > highest) {
+			highest = scores[i];
+		}
+	}
+	return highest;
+}
+
+int main() {
+	int scores[5] {85, 92, 78, 90, 88};
+
+	std::cout << "Sum: " << calculateSum(scores, 5) << '\n'
+		  << "Average: " << calculateAverage(scores, 5) << '\n'
+		  << "Highest: " << findHighest(scores, 5) << '\n';
+
+	return 0;
+}
