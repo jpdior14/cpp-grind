5380572ff139f5a1b6ce508be0a0989b7ece5903
John-Peter Mbock <lipemlidjob.junior@ictuniversity.edu.cm>
2026-05-12 23:51:17 +0100

    Grade Manager v2.0

--- functions.cpp
+++ functions.cpp
@@ -33,13 +33,24 @@ int findLowest(int scores[], int size) {
 	return lowest;
 }
 
+int countAbove80(int scores[], int size) {
+	int count {0};
+	for (int i {0}; i < size; i++) {
+		if (scores[i] >= 80) {
+			count++;
+		}
+	}
+	return count;
+}
+
 int main() {
 	int scores[7] {85, 92, 78, 90, 88, 86, 87};
 
 	std::cout << "Sum: " << calculateSum(scores, 7) << '\n'
 		  << "Average: " << calculateAverage(scores, 7) << '\n'
 		  << "Highest: " << findHighest(scores, 7) << '\n'
-		  << "Lowest: " << findLowest(scores, 7) << '\n';
+		  << "Lowest: " << findLowest(scores, 7) << '\n'
+		  << "Number of scores with 80 and above: " << countAbove80(scores, 7) << '\n';
 
 	return 0;
 }
