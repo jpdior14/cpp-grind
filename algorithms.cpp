c063d7aa1c08faf3c20e7ea459521a423fa44fd2
John-Peter Mbock <lipemlidjob.junior@ictuniversity.edu.cm>
2026-05-15 18:24:52 +0100

    Algorithms

--- algorithms.cpp
+++ algorithms.cpp
@@ -9,10 +9,10 @@ int linearSearch(int scores[], int size, int target) {
 	return -1;
 }
 
-void swap(int &a, int &b) {
-	int temp {a};
-	a = b;
-	b = temp;
+void swap(int* a, int* b) {
+	int temp {*a};
+	*a = *b;
+	*b = temp;
 }
 
 void bubbleSort(int scores[], int size) {
@@ -21,7 +21,7 @@ void bubbleSort(int scores[], int size) {
 
 		for (int i {0}; i <= size - 1 - pass; i++) {
 			if (scores[i] > scores[i+1]) {
-				swap(scores[i], scores[i+1]);
+				swap(&scores[i], &scores[i+1]);
 			}
 			swapped = true;
 		}
